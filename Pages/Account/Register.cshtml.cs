23365b9710a5cd5a37ca57ddd24f996067088f95
estebansp-1 <juanesteban9283@gmail.com>
2025-05-16 21:33:23 -0500

    User record created

---
+++ Pages/Account/Register.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+
+namespace SupermarketWEB.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+
+        private readonly SupermarketContext _context;
+
+        public RegisterModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.User User { get; set; }
+
+        public string ErrorMessage { get; set; }
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == User.Email);
+            if (existingUser != null)
+            {
+                ErrorMessage = "Email is already registered";
+                return Page();
+            }
+
+            _context.Users.Add(User);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Account/Login");
+        }
+    }
+}
